94d27e07d5c3a1b783601f9a0ad3e78f48c79d9a
tomWeissi <t.weissenbacher@yahoo.at>
2021-12-01 23:36:40 +0100

    Minor changes (Update Meeting sunday)

--- PickUpApp/PickUpApp/Models/Person.cs
+++ PickUpApp/PickUpApp/Models/Person.cs
@@ -6,5 +6,22 @@ namespace PickUpApp.Models
 {
     internal class Person
     {
+        private string name;
+        private string mail;
+        private long telNr;
+        private double credit;
+
+        //constructor
+        public Person(string name) { }
+        public Person(string name, string mail) { }
+        public Person(string name, string mail, long telNr) { }
+
+        //string name { get; set; }
+        //public string mail { get; set; }
+        //public long telNr { get; set; }
+        //location
+        //public double credit { get; set; }
+        //delivery (list)
+
     }
 }
